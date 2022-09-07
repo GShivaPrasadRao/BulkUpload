1e19d6f72ac0d8734fd2fa7b80e23af2406ce9bf
GShivaPrasadRao <gshivaprasad333@gmail.com>
2022-09-07 08:01:48 +0530

    Committing the files

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Bulk Upload(CSV & Excel)
+
+This application helps in uploading the data into Database
+which is in CSV or Excel format.
+
+## Tech Stack
+
+**Server:** Spring Boot, Apache POI(Excel),Apache Commons(CSV)
+
+**Database:** MySQL
+
+**Tools** : Postman
+
+## Screenshots
+
+
+1.[![Upload-Excel.png](https://i.postimg.cc/0NWZbbYN/Upload-Excel.png)](https://postimg.cc/CZ8b2M6y)
+
+2.[![Upload-CSV.png](https://i.postimg.cc/cHvhY0hB/Upload-CSV.png)](https://postimg.cc/XG096MTr)
+
+3.[![Dowload-Excel.png](https://i.postimg.cc/Jh4PrbBg/Dowload-Excel.png)](https://postimg.cc/4YqzwHPb)
+
+4.[![GetData.png](https://i.postimg.cc/76jVndQ8/GetData.png)](https://postimg.cc/r0Cx8nkQ)
---
+++ src/main/java/com/buildtechknowledge/spring/files/excel/service/CSVService.java
@@ -0,0 +1,28 @@
+package com.buildtechknowledge.spring.files.excel.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.buildtechknowledge.spring.files.excel.helper.CSVHelper;
+import com.buildtechknowledge.spring.files.excel.model.Tutorial;
+import com.buildtechknowledge.spring.files.excel.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class CSVService {
+    @Autowired
+    TutorialRepository repository;
+    public void save(MultipartFile file) {
+        try {
+            List<Tutorial> tutorials = CSVHelper.csvToTutorials(file.getInputStream());
+            repository.saveAll(tutorials);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+    public List<Tutorial> getAllTutorials() {
+        return repository.findAll();
+    }
+}
